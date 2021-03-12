840c7c86fb52dbfcdb266ebccda5c69c6d3a8f95
tiagombp <tiagombp@gmail.com>
2021-03-12 12:20:37 -0300

    posicionando os rects

--- experimentos/grid_1/vis.js
+++ experimentos/grid_1/vis.js
@@ -5,11 +5,13 @@ const vis = {
         unidade : {
             
             valor: +1e9,
-            tamanho: 5,
-            margem: 1,
-            qde_por_linha : 10
+            tamanho: 10,
+            margem: 4,
+            qde_por_linha : 20
 
-        }
+        },
+
+        espaco_inicial: 400
 
     },
 
@@ -23,11 +25,14 @@ const vis = {
     dims : {
 
         largura_necessaria : null,
-        altura_necessaria : null
+        altura_necessaria : null,
+        qde_unidades : null
 
     },
 
     data : {
+
+        divida : []
 
     },
 
@@ -44,8 +49,9 @@ const vis = {
 
             }
 
-            vis.dims.altura_necessaria  = dimensao_necessaria(qde_linhas);
+            vis.dims.altura_necessaria  = dimensao_necessaria(qde_linhas) + vis.params.espaco_inicial;
             vis.dims.largura_necessaria = dimensao_necessaria(vis.params.unidade.qde_por_linha);
+            vis.dims.qde_unidades       = qde_unidades;
 
             console.log("Precisaremos de ", qde_unidades, " unidades, ", qde_linhas, " linhas, e uma largura de ", dimensao_necessaria(vis.params.unidade.qde_por_linha), "px e uma altura de ", dimensao_necessaria(qde_linhas), "px.");
 
@@ -59,29 +65,89 @@ const vis = {
             cont.style("width", vis.dims.largura_necessaria + "px");
             svg.style("height", vis.dims.altura_necessaria + "px");
 
+        },
+
+        cria_dataset : function() {
+
+            for (let unidade = 1; unidade <= vis.dims.qde_unidades; unidade++) {
+
+                vis.data.divida[unidade-1] = {
+
+                    unidade : unidade,
+                    pos_x : ( (unidade - 1) % vis.params.unidade.qde_por_linha ) + 1,
+                    pos_y : Math.floor( (unidade - 1) / vis.params.unidade.qde_por_linha ) + 1
+
+                }
+
+            }
+
+
 
         },
 
-        cria : function() {
-
-            vis.grid.calcula(+4.8e11);
-            vis.grid.dimensiona_container();
-
-
-            // 
-
-
-
-        }
-
     },
 
-    init : function() {
+    draw : {
 
-        vis.grid.cria();
+        components : {
+
+            scales : {
+
+                x : function(pos_x) {
+
+                    const x = (pos_x - 1)*(vis.params.unidade.tamanho + vis.params.unidade.margem) + vis.params.unidade.margem;
+
+                    console.log(pos_x, x);
+
+                    return x
+
+                },
+
+                y : function(pos_y) {
+
+                    const y = vis.dims.altura_necessaria - pos_y*(vis.params.unidade.tamanho + vis.params.unidade.margem);
+
+                    console.log(pos_y, y)
+
+                    return y
+
+                }
+
+
+            }
+
+        },
+
+        desenhas_rects : function() {
+
+            const svg = d3.select(vis.refs.svg);
+
+            svg
+              .selectAll("rect")
+              .data(vis.data.divida)
+              .join("rect")
+              .classed("estoque", true)
+              .attr("x", d => vis.draw.components.scales.x(d.pos_x))
+              .attr("y", d => vis.draw.components.scales.y(d.pos_y))
+              .attr("width", vis.params.unidade.tamanho)
+              .attr("height", vis.params.unidade.tamanho);
+
+        }
+    },
+
+    control : {
+
+        init : function() {
+
+            vis.grid.calcula(+4.8e12);
+            vis.grid.dimensiona_container();
+            vis.grid.cria_dataset();
+            vis.draw.desenhas_rects();
+
+        }
 
     }
 
 }
 
-vis.init();
+vis.control.init();
