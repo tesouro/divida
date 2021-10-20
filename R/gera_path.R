96b2a562c8439c2cc107144efb757059a93bdc6f
tiagombp <tiagombp@gmail.com>
2021-10-20 16:53:57 -0300

    gera padrão para ser usado como separator

--- R/gera_path.R
+++ R/gera_path.R
@@ -44,3 +44,50 @@ proc_with_piece_info <- proc_treated %>%
   left_join(piece_info)
 
 jsonlite::write_json(proc_with_piece_info, '../abertura.json')
+
+
+# separator ---------------------------------------------------------------
+
+sep <- read_excel('prototipo-titulo-tetris.xlsx', sheet = 'separator')
+
+sep_proc <- data.frame()
+n <- 1
+
+for (i in 1:nrow(sep)) {
+  
+  for (j in 1:ncol(sep)) {
+    
+    if (!is.na(sep[i,j])) {
+      
+      sep_proc[n, "piece"] <- n
+      sep_proc[n, "x"] <- j-1
+      sep_proc[n, "y"] <- i-1
+      
+      n <- n+1
+      
+    }
+    
+  }
+}
+
+sep_proc_treated <- sep_proc %>%
+  arrange(piece) %>%
+  mutate(d = paste(
+    "M", x, y,
+    "H", x + 1,
+    "V", y + 1,
+    "H", x,
+    "V", y
+  )) %>%
+  group_by(piece) %>%
+  summarise(
+    d = paste(d, collapse = ' ')
+  ) %>%
+  ungroup() %>%
+  mutate(
+    element = paste0('<path d="',d,'"></path>')
+  )
+
+sep_out <- sep_proc_treated$element %>% paste(collapse = '\n')
+
+write_file(sep_out, file = "sep_out.html")
